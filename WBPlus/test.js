ff69d9c66f3b5be5ebbd19fcc503800296e2b499
yotsuki <yotsuki@vip.qq.com>
2013-07-28 14:59:10 +0800

    bug

---
+++ WBPlus/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: yotsuki
+ * Date: 13-7-23
+ * Time: 下午6:38
+ * To change this template use File | Settings | File Templates.
+ */
+function testMain(){
+    alert('this is test js!');
+}
+
+testMain();
